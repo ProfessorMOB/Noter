6eb81574de19ddb272a2b34bea6892bc49a1653e
Omar Mobayed <77638319+ProfessorMOB@users.noreply.github.com>
2021-11-11 03:38:34 -0500

    Fixed a f***ing stupid bug

---
+++ Noter/src/main/java10/org/bloomfield/noter/App.java
@@ -0,0 +1,20 @@
+package org.bloomfield.noter;
+
+import javax.swing.JFrame;
+import org.bloomfield.noter.utilities.*;
+public class App {
+    public static void main(String[] args )
+    {
+        
+        PathUtils PPF = new PathUtils();
+        String path = PPF.getDir();
+        System.out.println(path);
+
+        initApp app = new initApp();
+        app.JF(app.beginPage, 600);
+        app.beginPage.setTitle("Main Menu");
+        app.beginPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.initBeginPage();
+        app.beginPage.setVisible(true);
+    }
+}
